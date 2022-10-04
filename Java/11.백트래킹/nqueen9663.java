13c990aaa05dc88b2e3eef5f62fe70b10f283287
jungboke <tmdqjaqhdl3@naver.com>
2022-10-04 22:00:30 +0900

    싸피1차과정 복습

--- Java/11.백트래킹/nqueen9663.java
+++ Java/11.백트래킹/nqueen9663.java
@@ -1,41 +1,87 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//
+//	static boolean[] col = new boolean[16];
+//	// \
+//	static boolean[] dig1 = new boolean[32];
+//	// /
+//	static boolean[] dig2 = new boolean[32];
+//	static int answer = 0;
+//	static int N;
+//
+//	static void dfs(int row) {
+//		if(row==N) {
+//			answer+=1;
+//			return;
+//		}
+//		for(int i=0;i<N;i++) {
+//			if(col[i]==false&&dig1[row-i+N-1]==false&&dig2[row+i]==false) {
+//				col[i] = true;
+//				dig1[row-i+N-1] = true;
+//				dig2[row+i] = true;
+//				dfs(row+1);
+//				dig2[row+i] = false;
+//				dig1[row-i+N-1] = false;
+//				col[i] = false;
+//			}
+//		}
+//	}
+//
+//	public static void main(String[] args) throws NumberFormatException, IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//
+//		int n = Integer.parseInt(br.readLine());
+//		N=n;
+//		dfs(0);
+//		System.out.println(sb.append(answer));
+//	}
+//}
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static boolean[] col = new boolean[16];
-	// \
-	static boolean[] dig1 = new boolean[32];
-	// /
-	static boolean[] dig2 = new boolean[32];
 	static int answer = 0;
+	static boolean[] col = new boolean[15];
+	static boolean[] r_dig = new boolean[31];
+	static boolean[] l_dig = new boolean[31];
 	static int N;
-
-	static void dfs(int row) {
-		if(row==N) {
+	
+	static boolean queen_check(int r, int c) {
+		if(col[c]==true) return false;
+		if(r_dig[r-c+N-1]==true) return false;
+		if(l_dig[r+c]==true) return false;
+		return true;
+	}
+	
+	static void dfs(int r) {
+		if(r==N) {
 			answer+=1;
 			return;
 		}
 		for(int i=0;i<N;i++) {
-			if(col[i]==false&&dig1[row-i+N-1]==false&&dig2[row+i]==false) {
-				col[i] = true;
-				dig1[row-i+N-1] = true;
-				dig2[row+i] = true;
-				dfs(row+1);
-				dig2[row+i] = false;
-				dig1[row-i+N-1] = false;
-				col[i] = false;
-			}
+			if(!queen_check(r,i)) continue;
+			col[i] = true;
+			r_dig[r-i+N-1] = true;
+			l_dig[r+i] = true;
+			dfs(r+1);
+			l_dig[r+i] = false;
+			r_dig[r-i+N-1] = false;
+			col[i] = false;
 		}
 	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		
 		int n = Integer.parseInt(br.readLine());
 		N=n;
 		dfs(0);
--- Java/11.백트래킹/빵집3109.java
+++ Java/11.백트래킹/빵집3109.java
@@ -1,17 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.Comparator;
+//import java.util.LinkedList;
+//import java.util.List;
+//import java.util.Queue;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//	
+//	static int[] dx = {-1,0,1};
+//	static int[] dy = {1,1,1};
+//	static char[][] board = new char[10001][501];
+//	static boolean[][] check = new boolean[10001][501];
+//	static int N,M;
+//	static int answer = 0;
+//	
+//	static boolean dfs(int x, int y, int limit) {
+//		check[x][y] = true;
+//		if(y==limit-1) {
+//			answer+=1;
+//			return true;
+//		}
+//		for(int i=0;i<3;i++) {
+//			int nx = x+dx[i];
+//			int ny = y+dy[i];
+//			if(nx>=0&&nx<N&&ny>=0&&ny<M&&check[nx][ny]==false) {
+//				if(board[nx][ny]!='x') {
+//					if(dfs(nx,ny,limit)) return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+//	
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//		
+//		st = new StringTokenizer(br.readLine());
+//		int n = Integer.parseInt(st.nextToken());
+//		int m = Integer.parseInt(st.nextToken());
+//		N=n;M=m;
+//		for(int i=0;i<n;i++) {
+//			String temp = br.readLine();
+//			for(int j=0;j<m;j++) {
+//				board[i][j] = temp.charAt(j);
+//			}
+//		}
+//		for(int i=0;i<n;i++) {
+//			dfs(i,0,m);
+//		}
+//		System.out.println(sb.append(answer));
+//	}
+//}
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
 	static int[] dx = {-1,0,1};
 	static int[] dy = {1,1,1};
 	static char[][] board = new char[10001][501];
@@ -19,9 +70,9 @@ public class Main {
 	static int N,M;
 	static int answer = 0;
 	
-	static boolean dfs(int x, int y, int limit) {
+	static boolean dfs(int x, int y) {
 		check[x][y] = true;
-		if(y==limit-1) {
+		if(y==M-1) {
 			answer+=1;
 			return true;
 		}
@@ -30,7 +81,7 @@ public class Main {
 			int ny = y+dy[i];
 			if(nx>=0&&nx<N&&ny>=0&&ny<M&&check[nx][ny]==false) {
 				if(board[nx][ny]!='x') {
-					if(dfs(nx,ny,limit)) return true;
+					if(dfs(nx,ny)) return true;
 				}
 			}
 		}
@@ -53,7 +104,7 @@ public class Main {
 			}
 		}
 		for(int i=0;i<n;i++) {
-			dfs(i,0,m);
+			dfs(i,0);
 		}
 		System.out.println(sb.append(answer));
 	}
