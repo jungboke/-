6de8257e0787852169cf0a893f99ffc0a956e0ed
jungboke <tmdqjaqhdl3@naver.com>
2022-10-05 09:44:24 +0900

    싸피1차과정 복습

--- Java/10.이분탐색/숫자카드2 10816.java
+++ Java/10.이분탐색/숫자카드2 10816.java
@@ -1,13 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.LinkedList;
+//import java.util.List;
+//import java.util.Queue;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//	
+//	static List<Integer> a = new ArrayList<>();
+//	
+//	static int lower_bound(List<Integer> temp, int target) {
+//		int start = 0;
+//		int end = temp.size();
+//		while(start<end) {
+//			int mid = (start+end)/2;
+//			if(temp.get(mid)>=target) {
+//				end = mid;
+//			} else start = mid+1;
+//		}
+//		return end;
+//	}
+//	
+//	static int upper_bound(List<Integer> temp, int target) {
+//		int start = 0;
+//		int end = temp.size();
+//		while(start<end) {
+//			int mid = (start+end)/2;
+//			if(temp.get(mid)<=target) {
+//				start = mid+1;
+//			} else end = mid;
+//		}
+//		return end;
+//	}
+//	
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//		
+//		int n = Integer.parseInt(br.readLine());
+//		st = new StringTokenizer(br.readLine());
+//		for(int i=0;i<n;i++) {
+//			int x = Integer.parseInt(st.nextToken());
+//			a.add(x);
+//		}
+//		Collections.sort(a);
+//		int m = Integer.parseInt(br.readLine());
+//		st = new StringTokenizer(br.readLine());
+//		for(int i=0;i<m;i++) {
+//			int x = Integer.parseInt(st.nextToken());
+//			int idx1 = upper_bound(a,x);
+//			int idx2 = lower_bound(a,x);
+//			sb.append(idx1-idx2+" ");
+//		}
+//		System.out.println(sb);
+//	}
+//}
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
@@ -37,7 +91,7 @@ public class Main {
 		return end;
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
@@ -53,9 +107,9 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<m;i++) {
 			int x = Integer.parseInt(st.nextToken());
-			int idx1 = upper_bound(a,x);
-			int idx2 = lower_bound(a,x);
-			sb.append(idx1-idx2+" ");
+			int idx = lower_bound(a,x);
+			sb.append(upper_bound(a,x)-lower_bound(a,x)+" ");
+			
 		}
 		System.out.println(sb);
 	}
