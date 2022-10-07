01f062412f7952cbc8896d7d0b35673aad878d08
jungboke <tmdqjaqhdl3@naver.com>
2022-10-07 09:11:14 +0900

    싸피1차과정 복습

--- Java/3.greedy/행복유치원13164.java
+++ Java/3.greedy/행복유치원13164.java
@@ -1,36 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//
+//		st = new StringTokenizer(br.readLine());
+//		int n = Integer.parseInt(st.nextToken());
+//		int k = Integer.parseInt(st.nextToken());
+//		List<Integer> a = new ArrayList<>();
+//		st = new StringTokenizer(br.readLine());
+//		for(int i=0;i<n;i++) {
+//			int x = Integer.parseInt(st.nextToken());
+//			a.add(x);
+//		}
+//		List<Integer> diff = new ArrayList<>();
+//		for(int i=0;i<n-1;i++) {
+//			diff.add(a.get(i+1)-a.get(i));
+//		}
+//		Collections.sort(diff);
+//		int answer = 0;
+//		for(int i=0;i<diff.size()-k+1;i++) {
+//			answer += diff.get(i);
+//		}
+//		System.out.println(sb.append(answer + "\n"));
+//	}
+//}
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
+	
+	static List<Integer> a = new ArrayList<>();
+	static List<Integer> diff = new ArrayList<>();
+	static int answer = 0;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		List<Integer> a = new ArrayList<>();
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<n;i++) {
 			int x = Integer.parseInt(st.nextToken());
 			a.add(x);
 		}
-		List<Integer> diff = new ArrayList<>();
-		for(int i=0;i<n-1;i++) {
-			diff.add(a.get(i+1)-a.get(i));
+		for(int i=1;i<n;i++) {
+			int x = a.get(i)-a.get(i-1);
+			diff.add(x);
 		}
-		Collections.sort(diff);
-		int answer = 0;
-		for(int i=0;i<diff.size()-k+1;i++) {
-			answer += diff.get(i);
+		Collections.sort(diff,Collections.reverseOrder());
+		for(int i=k-1;i<diff.size();i++) {
+			answer+=diff.get(i);
 		}
-		System.out.println(sb.append(answer + "\n"));
+		System.out.println(sb.append(answer));
 	}
 }
--- Java/3.greedy/회의실배정1931.java
+++ Java/3.greedy/회의실배정1931.java
@@ -1,24 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.Comparator;
+//import java.util.LinkedList;
+//import java.util.List;
+//import java.util.Queue;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//
+//	static List<int[]> a = new ArrayList<>();
+//
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//
+//		int n = Integer.parseInt(br.readLine());
+//		for(int i=0;i<n;i++) {
+//			st = new StringTokenizer(br.readLine());
+//			int x = Integer.parseInt(st.nextToken());
+//			int y = Integer.parseInt(st.nextToken());
+//			a.add(new int[] {x,y});
+//		}
+//		Collections.sort(a, new Comparator<int[]>() {
+//			public int compare(int[] x, int[] y) {
+//				if(x[1]==y[1]) return Integer.compare(x[0], y[0]);
+//				return Integer.compare(x[1], y[1]);
+//			}
+//		});
+//		int answer = 0;
+//		int end = -1;
+//		for(int i=0;i<a.size();i++) {
+//			if(a.get(i)[0]>=end) {
+//				answer+=1;
+//				end = a.get(i)[1];
+//			}
+//		}
+//		System.out.println(sb.append(answer));
+//	}
+//}
+//
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+	
 	static List<int[]> a = new ArrayList<>();
-
-	public static void main(String[] args) throws IOException {
+	static int answer = 0;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		
 		int n = Integer.parseInt(br.readLine());
 		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
@@ -26,21 +64,19 @@ public class Main {
 			int y = Integer.parseInt(st.nextToken());
 			a.add(new int[] {x,y});
 		}
-		Collections.sort(a, new Comparator<int[]>() {
-			public int compare(int[] x, int[] y) {
-				if(x[1]==y[1]) return Integer.compare(x[0], y[0]);
-				return Integer.compare(x[1], y[1]);
+		Collections.sort(a,new Comparator<int[]>() {
+			public int compare(int[] o1, int[] o2) {
+				if(o1[1]==o2[1]) return o1[0]-o2[0];
+				return o1[1]-o2[1];
 			}
 		});
-		int answer = 0;
-		int end = -1;
-		for(int i=0;i<a.size();i++) {
-			if(a.get(i)[0]>=end) {
+		int start = 0;
+		for(int i=0;i<n;i++) {
+			if(start<=a.get(i)[0]) {
 				answer+=1;
-				end = a.get(i)[1];
+				start = a.get(i)[1];
 			}
 		}
 		System.out.println(sb.append(answer));
 	}
 }
-
