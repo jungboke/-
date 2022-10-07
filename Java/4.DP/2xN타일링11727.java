7ccf9df06b2d7aa8564368649776378c2bd374e4
jungboke <tmdqjaqhdl3@naver.com>
2022-10-07 18:02:30 +0900

    싸피1차과정 복습

--- Java/4.DP/2xN타일링11727.java
+++ Java/4.DP/2xN타일링11727.java
@@ -1,24 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//
+//	static int[] d = new int[1001];
+//
+//	public static void main(String[] args) throws Exception, IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//
+//		int n = Integer.parseInt(br.readLine());
+//		d[0] = 1;
+//		d[1] = 1;
+//		for(int i=2;i<=n;i++) {
+//			d[i] = d[i-1] + 2*d[i-2];
+//			d[i]%=10007;
+//		}
+//		System.out.println(sb.append(d[n] + "\n"));
+//	}
+//}
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+	
 	static int[] d = new int[1001];
-
-	public static void main(String[] args) throws Exception, IOException {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		
 		int n = Integer.parseInt(br.readLine());
 		d[0] = 1;
 		d[1] = 1;
 		for(int i=2;i<=n;i++) {
-			d[i] = d[i-1] + 2*d[i-2];
-			d[i]%=10007;
+			d[i] = (d[i-1]+2*d[i-2])%10007;
 		}
-		System.out.println(sb.append(d[n] + "\n"));
+		System.out.println(sb.append(d[n]));
 	}
 }
--- Java/4.DP/다리놓기1010.java
+++ Java/4.DP/다리놓기1010.java
@@ -1,34 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//	
+//	static int[][] d = new int[31][31];
+//	
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//		
+//		int t = Integer.parseInt(br.readLine());
+//		for(int i=1;i<=30;i++) {
+//			d[i][i] = 1;
+//			d[i][1] = i;
+//		}
+//		for(int i=2;i<=30;i++) {
+//			for(int j=i+1;j<=30;j++) {
+//				d[j][i] = d[j-1][i-1]+d[j-1][i];
+//			}
+//		}
+//		while(t-->0) {
+//			st = new StringTokenizer(br.readLine());
+//			int n = Integer.parseInt(st.nextToken());
+//			int m = Integer.parseInt(st.nextToken());
+//			sb.append(d[m][n]+"\n");
+//		}
+//		System.out.println(sb);
+//	}
+//}
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
 	static int[][] d = new int[31][31];
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		s
-		int t = Integer.parseInt(br.readLine());
+		
 		for(int i=1;i<=30;i++) {
 			d[i][i] = 1;
 			d[i][1] = i;
 		}
+		
 		for(int i=2;i<=30;i++) {
 			for(int j=i+1;j<=30;j++) {
-				d[j][i] = d[j-1][i-1]+d[j-1][i];
+				d[j][i] = d[j-1][i] + d[j-1][i-1];
 			}
 		}
+		int t = Integer.parseInt(br.readLine());
 		while(t-->0) {
 			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			sb.append(d[m][n]+"\n");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			sb.append(d[y][x]+"\n");
 		}
 		System.out.println(sb);
 	}
