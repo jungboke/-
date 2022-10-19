9304e758696bde3a99024a8da4e17b37ca6a9931
jungboke <tmdqjaqhdl3@naver.com>
2022-10-19 18:01:13 +0900

    1019

---
+++ Java/5.투포인터/세용액2473.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static List<Integer> a = new ArrayList<>();
+	static int[] answer = new int[3];
+	static long mini = Long.MAX_VALUE;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			int x = Integer.parseInt(st.nextToken());
+			a.add(x);
+		}
+		Collections.sort(a);
+		outer:for(int i=0;i<n-2;i++) {
+			int start = i+1;
+			int end = n-1;
+			while(start<end) {
+				long sum = (long)a.get(i)+a.get(start)+a.get(end);
+				if(mini>(long)Math.abs(sum)) {
+					mini = (long)Math.abs(sum);
+					answer[0] = a.get(i); answer[1] = a.get(start); answer[2] = a.get(end);
+				}
+				if(sum<0) start+=1;
+				else if(sum>0) end-=1;
+				else break outer;
+			}
+
+		}
+		sb.append(answer[0]+" "+answer[1]+" "+answer[2]);
+		System.out.println(sb);
+	}
+}
