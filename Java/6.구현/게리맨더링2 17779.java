59613fd4e69c6be79116976202d9fa6311f8d05f
jungboke <tmdqjaqhdl3@naver.com>
2022-12-11 22:53:00 +0900

    2차 싸피과정 복습

--- Java/6.구현/게리맨더링2 17779.java
+++ Java/6.구현/게리맨더링2 17779.java
@@ -1,71 +1,172 @@
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//
+//	static int[][] board = new int[21][21];
+//	static int mini = Integer.MAX_VALUE;
+//	static int total = 0;
+//	static int N;
+//	
+//	static void cal(int x, int y, int left, int right) {
+//		boolean[][] bound = new boolean[N][N];
+//		for(int i=0;i<=left;i++) {
+//			bound[x+i][y-i] = true;
+//			bound[x+right+i][y+right-i] = true;
+//		}
+//		for(int i=0;i<=right;i++) {
+//			bound[x+i][y+i] = true;
+//			bound[x+left+i][y-left+i] = true;
+//		}
+//		List<Integer> temp = new ArrayList<>();
+//		int sum = 0;
+//		for(int i=0;i<x+left;i++) {
+//			for(int j=0;j<=y;j++) {
+//				if(bound[i][j]==true) break;
+//				sum+=board[i][j];
+//			}
+//		}
+//		temp.add(sum);
+//		sum = 0;
+//		for(int i=0;i<=x+right;i++) {
+//			for(int j=N-1;j>y;j--) {
+//				if(bound[i][j]==true) break;
+//				sum+=board[i][j];
+//			}
+//		}
+//		temp.add(sum);
+//		sum = 0;
+//		for(int i=x+left;i<N;i++) {
+//			for(int j=0;j<y-left+right;j++) {
+//				if(bound[i][j]==true) break;
+//				sum+=board[i][j];
+//			}
+//		}
+//		temp.add(sum);
+//		sum = 0;
+//		for(int i=x+right+1;i<N;i++) {
+//			for(int j=N-1;j>=y-left+right;j--) {
+//				if(bound[i][j]==true) break;
+//				sum+=board[i][j];
+//			}
+//		}
+//		temp.add(sum);
+//		int sum_sum = 0;
+//		for(int i=0;i<temp.size();i++) {
+//			sum_sum+=temp.get(i);
+//		}
+//		temp.add(total-sum_sum);
+//		mini = Math.min(mini, Collections.max(temp)-Collections.min(temp));
+//	} 
+//	
+//	public static void main(String[] args) throws NumberFormatException, IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//		
+//		int n = Integer.parseInt(br.readLine());
+//		N=n;
+//		for(int i=0;i<n;i++) {
+//			st = new StringTokenizer(br.readLine());
+//			for(int j=0;j<n;j++) {
+//				board[i][j] = Integer.parseInt(st.nextToken());
+//				total+=board[i][j];
+//			}
+//		}
+//		for(int i=0;i<n;i++) {
+//			for(int j=0;j<n;j++) {
+//				for(int k=1;k<n;k++) {
+//					for(int l=1;l<n;l++) {
+//						if(i+k+l>=n) continue;
+//						if(j-k<0||j+l>=n) continue;
+//						cal(i,j,k,l);
+//					}
+//				}
+//			}
+//		}
+//		System.out.println(sb.append(mini));
+//	}
+//}
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	
 	static int[][] board = new int[21][21];
-	static int mini = Integer.MAX_VALUE;
-	static int total = 0;
+	static boolean[][] check = new boolean[21][21];
+	static int answer = Integer.MAX_VALUE;
 	static int N;
 	
-	static void cal(int x, int y, int left, int right) {
-		boolean[][] bound = new boolean[N][N];
-		for(int i=0;i<=left;i++) {
-			bound[x+i][y-i] = true;
-			bound[x+right+i][y+right-i] = true;
+	static void make_bound(int x, int y, int c, int d) {
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) check[i][j] = false;
 		}
-		for(int i=0;i<=right;i++) {
-			bound[x+i][y+i] = true;
-			bound[x+left+i][y-left+i] = true;
+		for(int i=0;i<=c;i++) {
+			check[x+i][y-i] = true;
+			check[x+d+i][y+d-i] = true;
 		}
-		List<Integer> temp = new ArrayList<>();
-		int sum = 0;
-		for(int i=0;i<x+left;i++) {
-			for(int j=0;j<=y;j++) {
-				if(bound[i][j]==true) break;
-				sum+=board[i][j];
-			}
+		for(int i=0;i<=d;i++) {
+			check[x+i][y+i] = true;
+			check[x+c+i][y-c+i] = true;
 		}
-		temp.add(sum);
-		sum = 0;
-		for(int i=0;i<=x+right;i++) {
-			for(int j=N-1;j>y;j--) {
-				if(bound[i][j]==true) break;
-				sum+=board[i][j];
-			}
-		}
-		temp.add(sum);
-		sum = 0;
-		for(int i=x+left;i<N;i++) {
-			for(int j=0;j<y-left+right;j++) {
-				if(bound[i][j]==true) break;
-				sum+=board[i][j];
-			}
-		}
-		temp.add(sum);
-		sum = 0;
-		for(int i=x+right+1;i<N;i++) {
-			for(int j=N-1;j>=y-left+right;j--) {
-				if(bound[i][j]==true) break;
-				sum+=board[i][j];
-			}
-		}
-		temp.add(sum);
-		int sum_sum = 0;
-		for(int i=0;i<temp.size();i++) {
-			sum_sum+=temp.get(i);
-		}
-		temp.add(total-sum_sum);
-		mini = Math.min(mini, Collections.max(temp)-Collections.min(temp));
-	} 
+	}
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int cal(int x, int y, int c, int d, int total) {
+		make_bound(x,y,c,d);
+		List<Integer> result = new ArrayList<>();
+		//1
+		int sum = 0;
+		for(int i=0;i<x+c;i++) {
+			for(int j=0;j<=y;j++) {
+				if(check[i][j]==true) break;
+				sum+=board[i][j];
+			}
+		}
+		result.add(sum);
+		total-=sum;
+		//2
+		sum = 0;
+		for(int i=0;i<=x+d;i++) {
+			for(int j=N-1;j>y;j--) {
+				if(check[i][j]==true) break;
+				sum+=board[i][j];
+			}
+		}
+		result.add(sum);
+		total-=sum;
+		//3
+		sum = 0;
+		for(int i=x+c;i<N;i++) {
+			for(int j=0;j<y-c+d;j++) {
+				if(check[i][j]==true) break;
+				sum+=board[i][j];
+			}
+		}
+		result.add(sum);
+		total-=sum;
+		//4
+		sum = 0;
+		for(int i=x+d+1;i<N;i++) {
+			for(int j=N-1;j>=y-c+d;j--) {
+				if(check[i][j]==true) break;
+				sum+=board[i][j];
+			}
+		}
+		result.add(sum);
+		total-=sum;
+		//5
+		result.add(total);
+		return Collections.max(result)-Collections.min(result);
+	}
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
 		N=n;
+		int total = 0;
 		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<n;j++) {
@@ -75,15 +176,16 @@ public class Main {
 		}
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				for(int k=1;k<n;k++) {
-					for(int l=1;l<n;l++) {
-						if(i+k+l>=n) continue;
-						if(j-k<0||j+l>=n) continue;
-						cal(i,j,k,l);
+				for(int c=1;c<n;c++) {
+					for(int d=1;d<n;d++) {
+						if(i+c+d>=n) continue;
+						if(j-c<0||j+d>=n) continue;
+						answer = Math.min(answer, cal(i,j,c,d,total));
 					}
 				}
 			}
 		}
-		System.out.println(sb.append(mini));
+		System.out.println(sb.append(answer));
 	}
 }
+
--- Java/6.구현/경사로14890.java
+++ Java/6.구현/경사로14890.java
@@ -1,80 +1,156 @@
+//import java.io.*;
+//import java.util.*;
+//import java.util.Map.Entry;
+//
+//public class Main {
+//	
+//	static int[][] board = new int[101][101];
+//	static int answer = 0;
+//	
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st;
+//
+//    	st = new StringTokenizer(br.readLine());
+//    	int n = Integer.parseInt(st.nextToken());
+//    	int m = Integer.parseInt(st.nextToken());
+//    	for(int i=0;i<n;i++) {
+//    		st = new StringTokenizer(br.readLine());
+//    		for(int j=0;j<n;j++) {
+//    			board[i][j] = Integer.parseInt(st.nextToken());
+//    		}
+//    	}
+//    	outer:for(int i=0;i<n;i++) {
+//    		List<int[]> temp = new ArrayList<>();
+//    		int cnt = 1;
+//    		for(int j=1;j<n;j++) {
+//    			if(board[i][j]!=board[i][j-1]) {
+//    				temp.add(new int[] {board[i][j-1],cnt});
+//    				cnt = 1;
+//    			} else cnt+=1;
+//    		}
+//    		temp.add(new int[] {board[i][n-1],cnt});
+//    		int[] check = new int[temp.size()];
+//    		if(temp.size()==1) answer+=1;
+//    		else {
+//    			for(int j=1;j<temp.size();j++) {
+//    				int[] prev = temp.get(j-1); int[] now = temp.get(j);
+//    				if(Math.abs(prev[0]-now[0])>1) continue outer;
+//    				if(prev[0]>now[0]) {
+//    					if(check[j]+m>now[1]||now[1]<m) continue outer;
+//    					else check[j] += m;
+//    				} else if(prev[0]<now[0]) {
+//    					if(check[j-1]+m>prev[1]||prev[1]<m) continue outer;
+//    					else check[j-1] += m;
+//    				}
+//    			}
+//    			answer+=1;
+//    		}
+//    	}
+//    	outer:for(int j=0;j<n;j++) {
+//    		List<int[]> temp = new ArrayList<>();
+//    		int cnt = 1;
+//    		for(int i=1;i<n;i++) {
+//    			if(board[i][j]!=board[i-1][j]) {
+//    				temp.add(new int[] {board[i-1][j],cnt});
+//    				cnt = 1;
+//    			} else cnt+=1;
+//    		}
+//    		temp.add(new int[] {board[n-1][j],cnt});
+//    		int[] check = new int[temp.size()];
+//    		if(temp.size()==1) answer+=1;
+//    		else {
+//    			for(int i=1;i<temp.size();i++) {
+//    				int[] prev = temp.get(i-1); int[] now = temp.get(i);
+//    				if(Math.abs(prev[0]-now[0])>1) continue outer;
+//    				if(prev[0]>now[0]) {
+//    					if(check[i]+m>now[1]||now[1]<m) continue outer;
+//    					else check[i] += m;
+//    				} else if(prev[0]<now[0]) {
+//    					if(check[i-1]+m>prev[1]||prev[1]<m) continue outer;
+//    					else check[i-1] += m;
+//    				}
+//    			}
+//    			answer+=1;
+//    		}
+//    	}
+//    	System.out.println(sb.append(answer));
+//	}
+//}
 import java.io.*;
 import java.util.*;
-import java.util.Map.Entry;
 
 public class Main {
 	
 	static int[][] board = new int[101][101];
 	static int answer = 0;
+	static int N,L;
 	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-    	st = new StringTokenizer(br.readLine());
-    	int n = Integer.parseInt(st.nextToken());
-    	int m = Integer.parseInt(st.nextToken());
-    	for(int i=0;i<n;i++) {
-    		st = new StringTokenizer(br.readLine());
-    		for(int j=0;j<n;j++) {
-    			board[i][j] = Integer.parseInt(st.nextToken());
-    		}
-    	}
-    	outer:for(int i=0;i<n;i++) {
-    		List<int[]> temp = new ArrayList<>();
-    		int cnt = 1;
-    		for(int j=1;j<n;j++) {
-    			if(board[i][j]!=board[i][j-1]) {
-    				temp.add(new int[] {board[i][j-1],cnt});
-    				cnt = 1;
-    			} else cnt+=1;
-    		}
-    		temp.add(new int[] {board[i][n-1],cnt});
-    		int[] check = new int[temp.size()];
-    		if(temp.size()==1) answer+=1;
-    		else {
-    			for(int j=1;j<temp.size();j++) {
-    				int[] prev = temp.get(j-1); int[] now = temp.get(j);
-    				if(Math.abs(prev[0]-now[0])>1) continue outer;
-    				if(prev[0]>now[0]) {
-    					if(check[j]+m>now[1]||now[1]<m) continue outer;
-    					else check[j] += m;
-    				} else if(prev[0]<now[0]) {
-    					if(check[j-1]+m>prev[1]||prev[1]<m) continue outer;
-    					else check[j-1] += m;
-    				}
-    			}
-    			answer+=1;
-    		}
-    	}
-    	outer:for(int j=0;j<n;j++) {
-    		List<int[]> temp = new ArrayList<>();
-    		int cnt = 1;
-    		for(int i=1;i<n;i++) {
-    			if(board[i][j]!=board[i-1][j]) {
-    				temp.add(new int[] {board[i-1][j],cnt});
-    				cnt = 1;
-    			} else cnt+=1;
-    		}
-    		temp.add(new int[] {board[n-1][j],cnt});
-    		int[] check = new int[temp.size()];
-    		if(temp.size()==1) answer+=1;
-    		else {
-    			for(int i=1;i<temp.size();i++) {
-    				int[] prev = temp.get(i-1); int[] now = temp.get(i);
-    				if(Math.abs(prev[0]-now[0])>1) continue outer;
-    				if(prev[0]>now[0]) {
-    					if(check[i]+m>now[1]||now[1]<m) continue outer;
-    					else check[i] += m;
-    				} else if(prev[0]<now[0]) {
-    					if(check[i-1]+m>prev[1]||prev[1]<m) continue outer;
-    					else check[i-1] += m;
-    				}
-    			}
-    			answer+=1;
-    		}
-    	}
-    	System.out.println(sb.append(answer));
+	// how 0 행 / how 1 열
+	static void cal(int how, int idx) {
+		List<int[]> temp = new ArrayList<>();
+		int cnt = 1;
+		if(how==0) {
+			for(int j=1;j<N;j++) {
+				if(board[idx][j]!=board[idx][j-1]) {
+					temp.add(new int[] {board[idx][j-1],cnt});
+					cnt = 1;
+				} else cnt+=1;
+			}
+			temp.add(new int[] {board[idx][N-1],cnt});
+		} else if(how==1) {
+			for(int i=1;i<N;i++) {
+				if(board[i][idx]!=board[i-1][idx]) {
+					temp.add(new int[] {board[i-1][idx],cnt});
+					cnt = 1;
+				} else cnt+=1;
+			}
+			temp.add(new int[] {board[N-1][idx],cnt});
+		}
+		for(int i=1;i<temp.size();i++) {
+			if(Math.abs(temp.get(i)[0]-temp.get(i-1)[0])>1) return;
+			// 증가
+			if(temp.get(i)[0]-temp.get(i-1)[0]==1) {
+				if(temp.get(i-1)[1]>=L) {
+					int x = temp.get(i-1)[0];
+					int y = temp.get(i-1)[1];
+					temp.set(i-1, new int[] {x,y-L});
+				} else return;
+			}
+			// 감소
+			if(temp.get(i)[0]-temp.get(i-1)[0]==-1) {
+				if(temp.get(i)[1]>=L) {
+					int x = temp.get(i)[0];
+					int y = temp.get(i)[1];
+					temp.set(i, new int[] {x,y-L});
+				} else return;
+			}
+		}
+		answer+=1;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		N=n;
+		int l = Integer.parseInt(st.nextToken());
+		L=l;
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<n;j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<n;j++) {
+				cal(i,j);
+			}
+		}
+		System.out.println(sb.append(answer));
 	}
 }
