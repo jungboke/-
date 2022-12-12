ef850c5d06dddb6a75ca7e0862628cf040eb0388
jungboke <tmdqjaqhdl3@naver.com>
2022-12-12 19:42:04 +0900

    2차 싸피과정 복습

--- Java/6.구현/사다리조작15684.java
+++ Java/6.구현/사다리조작15684.java
@@ -1,72 +1,137 @@
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//
+//	static int[][] board = new int[50][50];
+//	static boolean flag = false;
+//	static int answer = Integer.MAX_VALUE;
+//	static int N,M,H;
+//	
+//	static boolean ladder_check() {
+//		for(int i=1;i<=N;i++) {
+//			int now = i;
+//			for(int j=1;j<=H;j++) {
+//				if(now>1&&now<N) {
+//					if(board[now][j]==1) now += 1;
+//					else if(board[now-1][j]==1) now-=1;
+//				} else if(now==N&&board[now-1][j]==1) {
+//					now -= 1;
+//				} else if(now==1&&board[now][j]==1) {
+//					now += 1;
+//				}
+//			}
+//			if(now!=i) return false;
+//		}
+//		return true;
+//	}
+//	
+//	static void dfs(int cnt, int target) {
+//		if(flag==true) return;
+//		if(cnt==target) {
+//			if(ladder_check()) {
+//				answer = target;
+//				flag = true;
+//			}
+//			return;
+//		}
+//		for(int i=1;i<=H;i++) {
+//			for(int j=1;j<N;j++) {
+//				if(board[j][i]==0&&board[j+1][i]==0&&board[j-1][i]==0) {
+//					board[j][i] = 1;
+//					dfs(cnt+1,target);
+//					board[j][i] = 0;					
+//				}
+//			}
+//		}
+//	}
+//	
+//	public static void main(String[] args) throws NumberFormatException, IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//		
+//		st = new StringTokenizer(br.readLine());
+//		int n = Integer.parseInt(st.nextToken());
+//		int m = Integer.parseInt(st.nextToken());
+//		int h = Integer.parseInt(st.nextToken());
+//		N=n;M=m;H=h;
+//		for(int i=0;i<m;i++) {
+//			st = new StringTokenizer(br.readLine());
+//			int x = Integer.parseInt(st.nextToken());
+//			int y = Integer.parseInt(st.nextToken());
+//			board[y][x] = 1;
+//		}
+//		for(int i=0;i<=3;i++) {
+//			dfs(0,i);
+//			if(flag==true) break;
+//		}
+//		if(answer==Integer.MAX_VALUE) answer = -1;
+//		System.out.println(sb.append(answer));
+//	}
+//}
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static int[][] board = new int[50][50];
-	static boolean flag = false;
-	static int answer = Integer.MAX_VALUE;
-	static int N,M,H;
+	
+	static int[][] board = new int[31][11];
+	static int answer = -1;
+	static int N,H;
 	
 	static boolean ladder_check() {
-		for(int i=1;i<=N;i++) {
-			int now = i;
-			for(int j=1;j<=H;j++) {
-				if(now>1&&now<N) {
-					if(board[now][j]==1) now += 1;
-					else if(board[now-1][j]==1) now-=1;
-				} else if(now==N&&board[now-1][j]==1) {
-					now -= 1;
-				} else if(now==1&&board[now][j]==1) {
-					now += 1;
-				}
+		for(int j=1;j<=N;j++) {
+			int now = j;
+			for(int i=1;i<=H;i++) {
+				if(board[i][now]==1) now+=1;
+				else if(board[i][now-1]==1) now-=1;
 			}
-			if(now!=i) return false;
+			if(now!=j) return false;
 		}
 		return true;
 	}
 	
-	static void dfs(int cnt, int target) {
-		if(flag==true) return;
-		if(cnt==target) {
+	static boolean dfs(int cnt, int limit) {
+		if(cnt==limit) {
 			if(ladder_check()) {
-				answer = target;
-				flag = true;
-			}
-			return;
+				return true;
+			} else return false;
 		}
-		for(int i=1;i<=H;i++) {
-			for(int j=1;j<N;j++) {
-				if(board[j][i]==0&&board[j+1][i]==0&&board[j-1][i]==0) {
-					board[j][i] = 1;
-					dfs(cnt+1,target);
-					board[j][i] = 0;					
-				}
+		for(int j=1;j<N;j++) {
+			for(int i=1;i<=H;i++) {
+				if(!(board[i][j-1]==0&&board[i][j]==0&&board[i][j+1]==0)) continue;
+				board[i][j] = 1;
+				if(dfs(cnt+1,limit)) return true;
+				board[i][j] = 0;
 			}
 		}
+		return false;
 	}
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
+		N=n;
 		int m = Integer.parseInt(st.nextToken());
 		int h = Integer.parseInt(st.nextToken());
-		N=n;M=m;H=h;
+		H=h;
 		for(int i=0;i<m;i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			board[y][x] = 1;
+			board[x][y] = 1;
 		}
 		for(int i=0;i<=3;i++) {
-			dfs(0,i);
-			if(flag==true) break;
+			if(dfs(0,i)) {
+				answer = i;
+				break;
+			}
 		}
-		if(answer==Integer.MAX_VALUE) answer = -1;
 		System.out.println(sb.append(answer));
 	}
 }
+
--- Java/6.구현/숫자고르기2668.java
+++ Java/6.구현/숫자고르기2668.java
@@ -1,3 +1,54 @@
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//	
+//	static int[] a = new int[101];
+//	static boolean[] check = new boolean[101];
+//	static int ans_cnt = -1;
+//	static List<Integer> answer = new ArrayList<>(); 
+//	
+//	static void dfs(int start, int num, List<Integer> temp) {
+//		if(num==start) {
+//			for(int i=0;i<temp.size();i++) {
+//				answer.add(temp.get(i));
+//			}
+//			return;
+//		}
+//		if(check[num]==false) {
+//			temp.add(num);
+//			check[num] = true;
+//			dfs(start,a[num],temp);
+//			check[num] = false;
+//			temp.remove(temp.size()-1);
+//		}
+//	}
+//	
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st;
+//
+//        int n = Integer.parseInt(br.readLine());
+//        for(int i=1;i<=n;i++) {
+//        	int x = Integer.parseInt(br.readLine());
+//        	a[i] = x;
+//        }
+//        List<Integer> temp = new ArrayList<>();
+//        for(int i=1;i<=n;i++) {
+//        	if(answer.contains(i)) continue;
+//        	temp.add(i);
+//        	check[i] = true;
+//        	dfs(i,a[i],temp);
+//        	check[i] = false;
+//        	temp.remove(temp.size()-1);
+//        }
+//        sb.append(answer.size()+"\n");
+//        Collections.sort(answer);
+//        for(int i=0;i<answer.size();i++) sb.append(answer.get(i)+"\n");
+//        System.out.println(sb);
+//	} 
+//}
 import java.io.*;
 import java.util.*;
 
@@ -5,47 +56,40 @@ public class Main {
 	
 	static int[] a = new int[101];
 	static boolean[] check = new boolean[101];
-	static int ans_cnt = -1;
-	static List<Integer> answer = new ArrayList<>(); 
+	static int answer = 0;
 	
-	static void dfs(int start, int num, List<Integer> temp) {
-		if(num==start) {
-			for(int i=0;i<temp.size();i++) {
-				answer.add(temp.get(i));
+	static boolean dfs(int now, int target, int cnt) {
+		if(check[now]==true) {
+			if(now==target) {
+				answer+=cnt;
+				return true;
 			}
-			return;
+			else return false;
 		}
-		if(check[num]==false) {
-			temp.add(num);
-			check[num] = true;
-			dfs(start,a[num],temp);
-			check[num] = false;
-			temp.remove(temp.size()-1);
-		}
+		check[now] = true;
+		if(dfs(a[now],target,cnt+1)) return true;
+		check[now] = false;
+		return false;
 	}
 	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int n = Integer.parseInt(br.readLine());
-        for(int i=1;i<=n;i++) {
-        	int x = Integer.parseInt(br.readLine());
-        	a[i] = x;
-        }
-        List<Integer> temp = new ArrayList<>();
-        for(int i=1;i<=n;i++) {
-        	if(answer.contains(i)) continue;
-        	temp.add(i);
-        	check[i] = true;
-        	dfs(i,a[i],temp);
-        	check[i] = false;
-        	temp.remove(temp.size()-1);
-        }
-        sb.append(answer.size()+"\n");
-        Collections.sort(answer);
-        for(int i=0;i<answer.size();i++) sb.append(answer.get(i)+"\n");
-        System.out.println(sb);
-	} 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i=1;i<=n;i++) {
+			int x = Integer.parseInt(br.readLine());
+			a[i] = x;
+		}
+		for(int i=1;i<=n;i++) {
+			if(check[i]==true) continue;
+			dfs(i,i,0);
+		}
+		sb.append(answer+"\n");
+		for(int i=1;i<=n;i++) {
+			if(check[i]==true) sb.append(i+"\n");
+		}
+		System.out.println(sb);
+	}
 }
