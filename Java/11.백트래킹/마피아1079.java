910aa9a9ac28e26ab15e5bdc0e6f7f2a309ec561
jungboke <tmdqjaqhdl3@naver.com>
2023-01-05 11:25:16 +0900

    싸피2차과정 복습

--- Java/11.백트래킹/마피아1079.java
+++ Java/11.백트래킹/마피아1079.java
@@ -1,14 +1,99 @@
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+	
+// 	static int[][] board = new int[17][17];
+// 	static int[] gp = new int[17];
+// 	static boolean[] check = new boolean[17];
+// 	static int answer = 0;
+// 	static int N,K;
+	
+	
+// 	static void dfs(int cnt, int day) {
+// 		if(cnt%2==0) {
+// 			if(cnt==2) {
+// 				answer = Math.max(answer, day+1);
+// 				return;
+// 			}
+// 			for(int i=0;i<N;i++) {
+// 				if(i==K) continue;
+// 				if(check[i]==true) continue;
+// 				check[i] = true;
+// 				for(int j=0;j<N;j++) {
+// 					gp[j]+=board[i][j];
+// 				}
+// 				dfs(cnt-1,day+1);
+// 				for(int j=0;j<N;j++) {
+// 					gp[j]-=board[i][j];
+// 				}
+// 				check[i] = false;
+// 			}
+// 		} else {
+// 			int maxi = Integer.MIN_VALUE;
+// 			int max_idx = -1;
+// 			for(int i=0;i<N;i++) {
+// 				if(check[i]==true) continue;
+// 				if(gp[i]>maxi) {
+// 					maxi = gp[i];
+// 					max_idx = i;
+// 				}
+// 			}
+// 			if(max_idx==K) {
+// 				answer = Math.max(answer, day);
+// 				return;
+// 			}
+// 			check[max_idx] = true;
+// 			dfs(cnt-1,day);
+// 			check[max_idx] = false;
+// 		}
+// 	}
+	
+// 	public static void main(String[] args) throws NumberFormatException, IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuilder sb = new StringBuilder();
+// 		StringTokenizer st;
+		
+// 		int n = Integer.parseInt(br.readLine());
+// 		N=n;
+// 		st = new StringTokenizer(br.readLine());
+// 		for(int i=0;i<n;i++) {
+// 			gp[i] = Integer.parseInt((st.nextToken()));
+// 		}
+// 		for(int i=0;i<n;i++) {
+// 			st = new StringTokenizer(br.readLine());
+// 			for(int j=0;j<n;j++) {
+// 				board[i][j] = Integer.parseInt(st.nextToken());
+// 			}
+// 		}
+// 		int k = Integer.parseInt(br.readLine());
+// 		K=k;
+// 		dfs(n,0);
+// 		System.out.println(sb.append(answer));
+// 	} 
+// }
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	
-	static int[][] board = new int[17][17];
-	static int[] gp = new int[17];
+	static int[] point = new int[17];
+	static int[][] a = new int[17][17];
 	static boolean[] check = new boolean[17];
-	static int answer = 0;
-	static int N,K;
+	static int answer = Integer.MIN_VALUE;
+	static int N,M;
 	
+	static void kill(int x, boolean flag) {
+		if(flag==true) {
+			for(int i=0;i<N;i++) {
+				point[i]+=a[x][i];
+			}
+		} else {
+			for(int i=0;i<N;i++) {
+				point[i]-=a[x][i];
+			}
+		}
+	}
 	
 	static void dfs(int cnt, int day) {
 		if(cnt%2==0) {
@@ -17,39 +102,36 @@ public class Main {
 				return;
 			}
 			for(int i=0;i<N;i++) {
-				if(i==K) continue;
+				if(i==M) continue;
 				if(check[i]==true) continue;
 				check[i] = true;
-				for(int j=0;j<N;j++) {
-					gp[j]+=board[i][j];
-				}
+				kill(i,true);
 				dfs(cnt-1,day+1);
-				for(int j=0;j<N;j++) {
-					gp[j]-=board[i][j];
-				}
+				kill(i,false);
 				check[i] = false;
-			}
+			}			
 		} else {
 			int maxi = Integer.MIN_VALUE;
-			int max_idx = -1;
+			int max_num = -1;
 			for(int i=0;i<N;i++) {
 				if(check[i]==true) continue;
-				if(gp[i]>maxi) {
-					maxi = gp[i];
-					max_idx = i;
+				if(maxi<point[i]) {
+					maxi = point[i];
+					max_num = i;
 				}
 			}
-			if(max_idx==K) {
+			if(max_num==M) {
 				answer = Math.max(answer, day);
 				return;
 			}
-			check[max_idx] = true;
+			check[max_num] = true;
 			dfs(cnt-1,day);
-			check[max_idx] = false;
+			check[max_num] = false;
 		}
+		
 	}
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
@@ -58,17 +140,18 @@ public class Main {
 		N=n;
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<n;i++) {
-			gp[i] = Integer.parseInt((st.nextToken()));
+			point[i] = Integer.parseInt(st.nextToken());
 		}
 		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<n;j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				a[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		int k = Integer.parseInt(br.readLine());
-		K=k;
+		int m = Integer.parseInt(br.readLine());
+		M=m;
 		dfs(n,0);
 		System.out.println(sb.append(answer));
-	} 
+	}
 }
+
--- Java/11.백트래킹/색종이붙이기17136.java
+++ Java/11.백트래킹/색종이붙이기17136.java
@@ -1,61 +1,140 @@
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+	
+// 	static int[][] board = new int[10][10];
+// 	static int[] sticker = new int[] {5,5,5,5,5};
+// 	static int answer = Integer.MAX_VALUE;
+	
+// 	static boolean stickCheck(int x, int y, int k) {
+// 		for(int i=x;i<x+k;i++) {
+// 			for(int j=y;j<y+k;j++) {
+// 				if(!(i>=0&&i<10&&j>=0&&j<10)) return false;
+// 				if(board[i][j]==0) return false;
+// 			}
+// 		}
+// 		return true;
+// 	}
+	
+// 	static void stick(int x, int y, int k) {
+// 		for(int i=x;i<x+k;i++) {
+// 			for(int j=y;j<y+k;j++) {
+// 				board[i][j] = 0;
+// 			}
+// 		}
+// 	}
+	
+// 	static void unstick(int x, int y, int k) {
+// 		for(int i=x;i<x+k;i++) {
+// 			for(int j=y;j<y+k;j++) {
+// 				board[i][j] = 1;
+// 			}
+// 		}
+// 	}
+	
+// 	static void dfs(int x, int y, int cnt) {
+// 		if(x==10) {
+// 			answer = Math.min(answer, cnt);
+// 			return;
+// 		}
+// 		if(y>9) {
+// 			dfs(x+1,0,cnt);
+// 			return;
+// 		}
+// 		if(board[x][y]==1) {
+// 			for(int k=1;k<=5;k++) {
+// 				if(sticker[k-1]>0&&stickCheck(x,y,k)) {
+// 					sticker[k-1]-=1;
+// 					stick(x,y,k);
+// 					dfs(x,y+1,cnt+1);
+// 					unstick(x,y,k);
+// 					sticker[k-1]+=1;
+// 				}
+// 			}
+// 		} else dfs(x,y+1,cnt);
+// 	}
+	
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuilder sb = new StringBuilder();
+// 		StringTokenizer st;
+		
+// 		for(int i=0;i<10;i++) {
+// 			st = new StringTokenizer(br.readLine());
+// 			for(int j=0;j<10;j++) {
+// 				board[i][j] = Integer.parseInt(st.nextToken());
+// 			}
+// 		}
+// 		dfs(0,0,0);
+// 		if(answer==Integer.MAX_VALUE) answer = -1;
+// 		System.out.println(sb.append(answer));
+// 	}
+// }
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	
 	static int[][] board = new int[10][10];
-	static int[] sticker = new int[] {5,5,5,5,5};
+	static int[] sticker = new int[6];
 	static int answer = Integer.MAX_VALUE;
 	
-	static boolean stickCheck(int x, int y, int k) {
-		for(int i=x;i<x+k;i++) {
-			for(int j=y;j<y+k;j++) {
-				if(!(i>=0&&i<10&&j>=0&&j<10)) return false;
-				if(board[i][j]==0) return false;
+	static boolean stickCheck(int x, int y, int l) {
+		for(int i=0;i<l;i++) {
+			for(int j=0;j<l;j++) {
+				if(!(x+i>=0&&x+i<10&&y+j>=0&&y+j<10)) return false;
+				if(board[x+i][y+j]==0) return false;
 			}
 		}
 		return true;
 	}
 	
-	static void stick(int x, int y, int k) {
-		for(int i=x;i<x+k;i++) {
-			for(int j=y;j<y+k;j++) {
-				board[i][j] = 0;
-			}
-		}
-	}
-	
-	static void unstick(int x, int y, int k) {
-		for(int i=x;i<x+k;i++) {
-			for(int j=y;j<y+k;j++) {
-				board[i][j] = 1;
-			}
-		}
-	}
-	
-	static void dfs(int x, int y, int cnt) {
-		if(x==10) {
-			answer = Math.min(answer, cnt);
-			return;
-		}
-		if(y>9) {
-			dfs(x+1,0,cnt);
-			return;
-		}
-		if(board[x][y]==1) {
-			for(int k=1;k<=5;k++) {
-				if(sticker[k-1]>0&&stickCheck(x,y,k)) {
-					sticker[k-1]-=1;
-					stick(x,y,k);
-					dfs(x,y+1,cnt+1);
-					unstick(x,y,k);
-					sticker[k-1]+=1;
+	static void stick(int x, int y, int l, boolean flag) {
+		if(flag==true) {
+			for(int i=0;i<l;i++) {
+				for(int j=0;j<l;j++) {
+					board[x+i][y+j] = 0;
 				}
 			}
-		} else dfs(x,y+1,cnt);
+		} else {
+			for(int i=0;i<l;i++) {
+				for(int j=0;j<l;j++) {
+					board[x+i][y+j] = 1;
+				}
+			}
+		}
 	}
 	
-	public static void main(String[] args) throws IOException {
+	static void dfs(int idx, int cnt) {
+		int x = idx/10; int y = idx%10;
+		if(idx==100) {
+			boolean flag = true;
+			for(int i=0;i<10;i++) {
+				for(int j=0;j<10;j++) {
+					if(board[i][j]==1) flag = false;
+				}
+			}
+			if(flag==true) {
+				answer = Math.min(answer, cnt);
+			}
+			return;
+		}
+		if(board[x][y]==0) dfs(idx+1,cnt);
+		else {
+			for(int i=1;i<=5;i++) {
+				if(stickCheck(x,y,i)&&sticker[i]<5) {
+					sticker[i]+=1;
+					stick(x,y,i,true);
+					dfs(idx+1,cnt+1);
+					stick(x,y,i,false);
+					sticker[i]-=1;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
@@ -66,7 +145,7 @@ public class Main {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dfs(0,0,0);
+		dfs(0,0);
 		if(answer==Integer.MAX_VALUE) answer = -1;
 		System.out.println(sb.append(answer));
 	}
