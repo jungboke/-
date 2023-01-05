8712c04a5dc2804d016244f463924c8ed8026edd
jungboke <tmdqjaqhdl3@naver.com>
2023-01-05 16:30:11 +0900

    싸피2차과정 복습

--- Java/10.이분탐색/랜선자르기1654.java
+++ Java/10.이분탐색/랜선자르기1654.java
@@ -1,38 +1,77 @@
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+// 	static List<Long> a = new ArrayList<>();
+// 	static long answer = -1;
+
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringBuilder sb = new StringBuilder();
+//         StringTokenizer st;
+
+//     	st = new StringTokenizer(br.readLine());
+//     	int n = Integer.parseInt(st.nextToken());
+//     	int m = Integer.parseInt(st.nextToken());
+//     	for(int i=0;i<n;i++) {
+//     		long x = Integer.parseInt(br.readLine());
+//     		a.add(x);
+//     	}
+//     	long start = 1;
+//     	long end = Collections.max(a);
+//     	while(start<=end) {
+//     		long mid = (start+end)/2;
+//     		long cnt = 0;
+//     		for(int i=0;i<a.size();i++) {
+//     			cnt += a.get(i)/mid;
+//     		}
+//     		if(cnt>=m) {
+//     			start = mid + 1;
+//     			answer = Math.max(answer, mid);
+//     		} else {
+//     			end = mid - 1;
+//     		}
+//     	}
+//     	System.out.println(sb.append(answer));
+// 	}
+// }
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static List<Long> a = new ArrayList<>();
-	static long answer = -1;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-    	st = new StringTokenizer(br.readLine());
-    	int n = Integer.parseInt(st.nextToken());
-    	int m = Integer.parseInt(st.nextToken());
-    	for(int i=0;i<n;i++) {
-    		long x = Integer.parseInt(br.readLine());
-    		a.add(x);
-    	}
-    	long start = 1;
-    	long end = Collections.max(a);
-    	while(start<=end) {
-    		long mid = (start+end)/2;
-    		long cnt = 0;
-    		for(int i=0;i<a.size();i++) {
-    			cnt += a.get(i)/mid;
-    		}
-    		if(cnt>=m) {
-    			start = mid + 1;
-    			answer = Math.max(answer, mid);
-    		} else {
-    			end = mid - 1;
-    		}
-    	}
-    	System.out.println(sb.append(answer));
+	
+	static List<Long> ran = new ArrayList<>();
+	static long answer = Integer.MIN_VALUE;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		for(int i=0;i<n;i++) {
+			long x = Integer.parseInt(br.readLine());
+			ran.add(x);
+		}
+		long start = 1;
+		long end = Collections.max(ran);
+		while(start<=end) {
+			long mid = (start+end)/2;
+			int cnt = 0;
+			for(int i=0;i<ran.size();i++) {
+				cnt += ran.get(i)/mid;
+			}
+			if(cnt>=k) {
+				answer = Math.max(answer, mid);
+				start = mid+1;
+			} else {
+				end = mid-1;
+			}
+		}
+		System.out.println(sb.append(answer));
 	}
 }
+
--- Java/12.분할정복/Z 1074.java
+++ Java/12.분할정복/Z 1074.java
@@ -41,30 +41,70 @@
 //		dfs(0,0,rn);
 //	}
 //}
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+	
+// 	static int R,C;
+	
+// 	static void dfs(int x, int y, int r, int cnt) {
+// 		if(r==1) {
+// 			System.out.println(cnt);
+// 			System.exit(0);
+// 		}
+// 		if(R>=x&&R<x+r/2&&C>=y&&C<y+r/2) {
+// 			dfs(x,y,r/2,cnt+r*r/4*0);
+// 		} else if(R>=x&&R<x+r/2&&C>=y+r/2&&C<y+r) {
+// 			dfs(x,y+r/2,r/2,cnt+r*r/4*1);
+// 		} else if(R>=x+r/2&&R<x+r&&C>=y&&C<y+r/2) {
+// 			dfs(x+r/2,y,r/2,cnt+r*r/4*2);
+// 		} else if(R>=x+r/2&&R<x+r&&C>=y+r/2&&C<y+r) {
+// 			dfs(x+r/2,y+r/2,r/2,cnt+r*r/4*3);
+// 		}
+// 	}
+	
+// 	public static void main(String[] args) throws NumberFormatException, IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuilder sb = new StringBuilder();
+// 		StringTokenizer st;
+		
+// 		st = new StringTokenizer(br.readLine());
+// 		int n = Integer.parseInt(st.nextToken());
+// 		int r = Integer.parseInt(st.nextToken());
+// 		int c = Integer.parseInt(st.nextToken());
+// 		R=r;C=c;
+// 		dfs(0,0,(int)Math.pow(2, n),0);
+// 	}
+// }
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	
 	static int R,C;
+	static int answer = -1;
 	
-	static void dfs(int x, int y, int r, int cnt) {
-		if(r==1) {
-			System.out.println(cnt);
-			System.exit(0);
+	static boolean dfs(int n, int x, int y, int num) {
+		if(x==R&&y==C) {
+			answer = num;
+			return true;
 		}
-		if(R>=x&&R<x+r/2&&C>=y&&C<y+r/2) {
-			dfs(x,y,r/2,cnt+r*r/4*0);
-		} else if(R>=x&&R<x+r/2&&C>=y+r/2&&C<y+r) {
-			dfs(x,y+r/2,r/2,cnt+r*r/4*1);
-		} else if(R>=x+r/2&&R<x+r&&C>=y&&C<y+r/2) {
-			dfs(x+r/2,y,r/2,cnt+r*r/4*2);
-		} else if(R>=x+r/2&&R<x+r&&C>=y+r/2&&C<y+r) {
-			dfs(x+r/2,y+r/2,r/2,cnt+r*r/4*3);
+		int l = (int)Math.pow(2,n-1);
+		int temp = (int)Math.pow(l,2);
+		if(R<x+l&&C<y+l) {
+			if(dfs(n-1,x,y,num)) return true;
+		} else if(R<x+l&&C>=y+l) {
+			if(dfs(n-1,x,y+l,num+temp*1)) return true;
+		} else if(R>=x+l&&C<y+l) {
+			if(dfs(n-1,x+l,y,num+temp*2)) return true;
+		} else if(R>=x+l&&C>=y+l) {
+			if(dfs(n-1,x+l,y+l,num+temp*3)) return true;
 		}
+		return false;
 	}
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
@@ -74,6 +114,7 @@ public class Main {
 		int r = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 		R=r;C=c;
-		dfs(0,0,(int)Math.pow(2, n),0);
+		dfs(n,0,0,0);
+		System.out.println(sb.append(answer));
 	}
 }
